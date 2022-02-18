cea8fa524df10d1b2d0546ea9d2b2c50f33e8c79
Rushi <53105730+rushibhosale@users.noreply.github.com>
2022-02-18 22:41:33 +0530

    [grid] toggle start and end node

--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -3,11 +3,11 @@ import "./Grid.css";
 import { Node } from "./Node/Node";
 
 //startNode row,col
-const START_NODE_ROW = 10;
-const START_NODE_COL = 20;
+let START_NODE_ROW = 10;
+let START_NODE_COL = 20;
 //Finish NOde row,col
-const FINISH_NODE_ROW = 1;
-const FINISH_NODE_COL = 30;
+let FINISH_NODE_ROW = 1;
+let FINISH_NODE_COL = 30;
 
 export class Grid extends Component {
 
@@ -15,15 +15,13 @@ export class Grid extends Component {
         super();
         this.state = {
             grid: [],
-            isMouseClicked: false
+            isMouseClicked: false,
+            isDraggingStart: false,
+            isDraggingFinsh: false
         }
-
         //set variable for  max rows and cols
         this.maxWidth = 50;
         this.maxHeight = 20;
-
-
-
     }
 
     componentDidMount() {
@@ -83,21 +81,43 @@ export class Grid extends Component {
     //mouse clicked
     handleMouseDownEvent(row, col) {
         this.setState({ isMouseClicked: true });
-        if ((row == START_NODE_ROW && col == START_NODE_COL) || (row == FINISH_NODE_ROW && col == FINISH_NODE_COL)) return;
-        console.log("mouse is realease", row, col);
-        const grid = toggleWall(this.state.grid, row, col);
-        this.setState({ grid });
+        if (row == START_NODE_ROW && col == START_NODE_COL) {
+            const grid = toggleStart(this.state.grid, row, col);
+            this.setState({ grid, isDraggingStart: true });
+        }
+        else if (row === FINISH_NODE_ROW && col === FINISH_NODE_COL) {
+            const grid = toggleFinsih(this.state.grid, row, col);
+            this.setState({ grid, isDraggingFinsh: true });
+        }
+        else {
+            console.log("mouse is realease", row, col);
+            const grid = toggleWall(this.state.grid, row, col);
+            this.setState({ grid });
+
+        }
+        // || (row == FINISH_NODE_ROW && col == FINISH_NODE_COL)) return;
     }
     handleMouseEnterEvent(row, col) {
         if (!this.state.isMouseClicked) return;
-        if ((row == START_NODE_ROW && col == START_NODE_COL) || (row == FINISH_NODE_ROW && col == FINISH_NODE_COL)) return;
-        console.log("mouse is realease", row, col);
-        const grid = toggleWall(this.state.grid, row, col);
-        this.setState({ grid });
+        if (this.state.isDraggingStart) {
+            const grid = toggleStart(this.state.grid, row, col);
+            this.setState({ grid });
+            console.log("start nide");
+        }
+        else if (this.state.isDraggingFinsh) {
+            const grid = toggleFinsih(this.state.grid, row, col);
+            this.setState({ grid });
+        }
+        else {
+            console.log("mouse is realease", row, col);
+            const grid = toggleWall(this.state.grid, row, col);
+            this.setState({ grid });
+
+        }
     }
     //mouse realeased
     handleMouseUpEvent(row, col) {
-        this.setState({ isMouseClicked: false })
+        this.setState({ isMouseClicked: false, isDraggingStart: false })
     }
 
 
@@ -124,3 +144,33 @@ const toggleWall = (grid, row, col) => {
     tempGrid[row][col] = newNode;
     return tempGrid;
 }
+//toggle StartNode
+const toggleStart = (grid, row, col) => {
+    const tempGrid = grid.slice();
+    const node = tempGrid[row][col];
+    grid[START_NODE_ROW][START_NODE_COL].isStartNode = false;
+    const newNode = {
+        ...node,
+        isStartNode: !node.isStartNode
+    }
+    tempGrid[row][col] = newNode;
+    START_NODE_ROW = row;
+    START_NODE_COL = col;
+    return tempGrid;
+}
+
+//toggle finish
+const toggleFinsih = (grid, row, col) => {
+    const tempGrid = grid.slice();
+    const node = tempGrid[row][col];
+    grid[FINISH_NODE_ROW][FINISH_NODE_COL].isFinishNode = false;
+    const newNode = {
+        ...node,
+        isFinishNode: !node.isFinishNode
+    }
+    tempGrid[row][col] = newNode;
+    FINISH_NODE_ROW = row;
+    FINISH_NODE_COL = col;
+    return tempGrid;
+}
+
