1bb178a380a278b0a263a50f8b115f2a5becfe8c
Rushi <53105730+rushibhosale@users.noreply.github.com>
2022-02-22 19:41:19 +0530

    [algorithms] A* algorithm ecludiean distance with 8 neighbor

--- src/Algorithms/UtilityFunctions.js
+++ src/Algorithms/UtilityFunctions.js
@@ -6,6 +6,10 @@ const getAllNeigbours = (node, grid) => {
     if (col !== 0) neighbours.push(grid[row][col - 1]);
     if (row !== 0) neighbours.push(grid[row - 1][col]);
 
+    if (row !== 0 && col !== 0) neighbours.push(grid[row - 1][col - 1]);
+    if (row !== 0 && col !== grid[0].length - 1) neighbours.push(grid[row - 1][col + 1]);
+    if (row !== grid.length - 1 && col !== 0) neighbours.push(grid[row + 1][col - 1]);
+    if (row !== grid.length - 1 && col !== grid[0].length - 1) neighbours.push(grid[row + 1][col + 1]);
 
     return neighbours.filter(
         (neighbour) => !neighbour.isWall && !neighbour.isVisited
